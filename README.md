f50d56e13abccccde991d9bb36c88d93c6e3d85c
LeBoiZ <103581847+LeBoiZ10@users.noreply.github.com>
2024-04-13 21:36:53 -0400

    some more updates

--- README.md
+++
@@ -1,8 +0,0 @@
-# COMP3005 Final Project V1
-
-### Name & ID
-- Name: Zade Ammar
-- Student ID: 101272215
-
-# YouTube Video Link (Bonus)
-https://youtu.be/Dtrvn4fQid4
--- dbexport.sql
+++ dbexport.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:485140d247e98eff47e29c43c7119ee40f20538def7f7ed7de5c01c083afe013
-size 169744360
+oid sha256:bccffb58b6ac9aed014c76ffa7c776f0edcc1c979e500d6f68c99ca1d1500ea8
+size 169747557
